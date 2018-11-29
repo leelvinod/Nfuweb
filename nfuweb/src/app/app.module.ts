6b9b298164e7160829c199ccce6a8a68e91db086
Leel Vinod <leelvinod20@gmail.com>
2018-11-29 07:44:06 +0530

    third commit on 29/11

--- nfuweb/src/app/app.module.ts
+++ nfuweb/src/app/app.module.ts
@@ -5,21 +5,30 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { NgxGalleryModule } from 'ngx-gallery';
-
-
-
-
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AdmissionComponent } from './admission/admission.component';
+import { News1Component } from './news1/news1.component';
+import { News2Component } from './news2/news2.component';
+import { News3Component } from './news3/news3.component';
+import { NewslistComponent } from './newslist/newslist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AdmissionComponent,
+    News1Component,
+    News2Component,
+    News3Component,
+    NewslistComponent,
   ],
   imports: [
     NgxGalleryModule,
     BrowserModule,
+    AppRoutingModule,
+    
     NgbModule, BrowserAnimationsModule,
     CalendarModule.forRoot({
       provide: DateAdapter,
